c3a577ba0e280fb1484866cb6d3a221dcafce543
Tyler Stanish <tystanish@gmail.com>
2018-12-14 13:59:05 -0500

    got jest to work with react-navigation and babel

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
   preset: 'jest-expo',
   transform: {
     '\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
-  }
+  },
+  "transformIgnorePatterns": [
+    "node_modules/(?!react-native|react-navigation)/"
+  ]
 };
